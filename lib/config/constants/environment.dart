8cc42d29b8e568d88dcbdd3a4554d0e0c82958b0
Juan Torres <378tech@gmail.com>
2023-08-28 23:53:20 -0500

    a route is drawn joining two points on the map

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -2,4 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String googleDriveApiKey = dotenv.env['GOOGLE_DRIVE_API_KEY']!;
+  static String directionsApiKey = dotenv.env['DIRECTIONS_API_KEY']!;
 }
--- lib/presentation/widgets/custom_google_map.dart
+++ lib/presentation/widgets/custom_google_map.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:histouric_mobile_frontend/config/config.dart';
 
 import '../../domain/entities/entities.dart';
 import '../providers/providers.dart';
@@ -22,6 +24,10 @@ class CustomGoogleMap extends ConsumerStatefulWidget {
 }
 
 class __MapViewState extends ConsumerState<CustomGoogleMap> {
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+
   @override
   void initState() {
     super.initState();
@@ -32,6 +38,7 @@ class __MapViewState extends ConsumerState<CustomGoogleMap> {
     final mapController = ref.watch(mapControllerProvider(context));
 
     return GoogleMap(
+      polylines: Set<Polyline>.of(polylines.values),
       markers: mapController.markersSet,
       mapType: MapType.normal,
       initialCameraPosition: CameraPosition(
@@ -41,19 +48,50 @@ class __MapViewState extends ConsumerState<CustomGoogleMap> {
       myLocationEnabled: true,
       zoomControlsEnabled: true,
       onMapCreated: (controller) {
-        ref
-            .read(mapControllerProvider(context).notifier)
-            .setMapController(controller);
-        if (widget.bics != null && widget.bics!.isNotEmpty) {
+        controller.getVisibleRegion().then((value) async {
           ref
               .read(mapControllerProvider(context).notifier)
-              .setMarkers(widget.bics!);
-        } else {
-          ref
-              .read(mapControllerProvider(context).notifier)
-              .loadBICsFromBICRepository();
-        }
+              .setMapController(controller);
+          if (widget.bics != null && widget.bics!.isNotEmpty) {
+            ref
+                .read(mapControllerProvider(context).notifier)
+                .setMarkers(widget.bics!);
+            await _getPolyline();
+          } else {
+            ref
+                .read(mapControllerProvider(context).notifier)
+                .loadBICsFromBICRepository();
+          }
+        });
       },
     );
+  }
+
+  _addPolyLine() {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+        polylineId: id, color: Colors.red, points: polylineCoordinates);
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  _getPolyline() async {
+    try {
+      PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        Environment.directionsApiKey,
+        PointLatLng(widget.bics![0].latitude, widget.bics![0].longitude),
+        PointLatLng(widget.bics![1].latitude, widget.bics![1].longitude),
+        travelMode: TravelMode.driving,
+        // wayPoints: [PolylineWayPoint(location: "Sabo, Yaba Lagos Nigeria")],
+      );
+      if (result.points.isNotEmpty) {
+        result.points.forEach((PointLatLng point) {
+          polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+        });
+      }
+      _addPolyLine();
+    } catch (e) {
+      print(e);
+    }
   }
 }
